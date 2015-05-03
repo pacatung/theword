c749be5183c7d54b78dc631399d60c70cda268e2
jcskyting <jcskyting@gmail.com>
2015-05-03 18:41:50 +0800

    user can login by devise-FB

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  # devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :des_user_session
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -12,7 +19,9 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-    resources :messages
+  resources :messages
+
+
 
   # Example resource route with options:
   #   resources :products do
