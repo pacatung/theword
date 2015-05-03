7c058ec4d7d47bceb6ac0d73dacc3451d7855d9c
BigBoss <pacatung@gmail.com>
2015-05-03 19:10:26 +0800

    add message text area

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,12 @@
 class MessagesController < ApplicationController
 
   def index
+    @messages = Message.all
+    @message = Message.new
+  end
 
+  def new
+    @message = Message.new
   end
 
 end
