9f8c631052ad58c3a56c1bd1a7c52e862621e5c5
BigBoss <pacatung@gmail.com>
2015-05-03 20:58:55 +0800

    message create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
 
   def index
-    @messages = Message.all
+    @messages = Message.all.order("id DESC")
     @message = Message.new
   end
 
@@ -9,4 +9,20 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
 
+  def create
+    @message = Message.new(mge_params)
+    @message.user = current_user
+    if @message.save
+      flash[:notice] = "message was successfully created!!"
+      redirect_to messages_path
+    else
+      render :action => :new
+    end
+  end
+
+private
+
+  def mge_params
+    params.require(:message).permit(:content, :delivery_date, :user_id, :status)
+  end
 end
