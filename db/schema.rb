06d18df9021440214b96678cbddcd1686b001bf5
jcskyting <jcskyting@gmail.com>
2015-05-08 20:10:21 +0800

    add user phone

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150508025449) do
+ActiveRecord::Schema.define(version: 20150508120519) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name",                     limit: 255,                null: false
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20150508025449) do
     t.string   "status",                 limit: 255
     t.string   "fb_token",               limit: 255
     t.datetime "fb_expires_at"
+    t.string   "phone",                  limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
