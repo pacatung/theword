b60f38b00f0bb87b95e4fd376a307cf05b45c617
BigBoss <pacatung@gmail.com>
2015-05-09 10:16:28 +0800

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150508120519) do
 
   create_table "contacts", force: :cascade do |t|
