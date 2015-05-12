643d23061de85c0421b5017acc0511012766a3df
BigBoss <pacatung@gmail.com>
2015-05-12 17:48:30 +0800

    change user first login behavior

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-  "/contacts/new"
+    if  current_user.messages.count && current_user.contacts.count == 0
+      "/contacts/new"
+    else
+      "/messages"
+    end
   end
 end
