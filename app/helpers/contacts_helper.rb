b9d60012ef44d677a2f7663f411b2d1c5d108b3c
jcskyting <jcskyting@gmail.com>
2015-05-12 16:18:33 +0800

    fix select bug

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -2,13 +2,13 @@ module ContactsHelper
 
   def options_for_receiver(user)
     r=[]
-    user.contacts.each do |c|
-      r << c if c.receivers.first
+    user.contacts.each do |contact|
+      r << contact if contact.receivers.first
     end
 
     {
-      "從現有收訊人選擇" => r.map{|c|[c.name,c.id]}
-      "從聯絡人選擇" => user.contacts.all.map{ |c| [c.name, c.id] },
+      "從現有收訊人選擇" => r.map{ |c| [c.name, c.id] },
+      "從聯絡人選擇" => user.contacts.all.map{ |c| [c.name, c.id] }
     }
   end
 
